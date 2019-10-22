4b5b11aaa5c894fa9b578df925fbcd000ab55cc3
Samir Parab <samirparab@yahoo.com>
2019-10-22 20:06:29 +0000

    new file timer1.js.

--- timer1.js
+++ timer1.js
@@ -1,11 +1,33 @@
 const argv = process.argv.slice(2);
 
 for (const num of argv) {
-  
-  setTimeout(() => {  
-    process.stdout.write(`${num} seconds\n`);
-    process.stdout.write('\x07');
-  }, num * 1000);
+
+  if(argv.length === 0) {
+    setTimeout(() => {  
+      console.log("no numbers provided.");
+      //process.stdout.write(`no numbers provided.\n`);
+      }, 0);
+  }
+
+  if(num !== Number) {
+    setTimeout(() => {  
+      process.stdout.write(`no numbers provided.\n`);
+      }, 0);
+  }
+
+  if(num <= 0) {
+    setTimeout(() => {  
+      process.stdout.write(`${num} seconds is not a valid number\n`);
+      }, num * 1000);
+  }
+
+  if(num > 0) {
+    setTimeout(() => {  
+      process.stdout.write(`${num} seconds is not an option\n`);
+      process.stdout.write('\x07');
+    }, num * 1000);
+  }
+
 };
 
 // output
